f78fcabb582a2820050ad3512e986286aa6eb71b
dgueudre <dgueudre@gmail.com>
2022-10-27 19:37:48 +0200

    fix: display edited date

--- src/App/View/post.view.php
+++ src/App/View/post.view.php
@@ -2,8 +2,8 @@
 <h2><?= $post->title; ?></h2>
 <p><?= $post->intro; ?></p>
 <p><?= $post->content; ?></p>
-<span>Créé le <?= $post->created_at->format('d/m/Y H:i:s'); ?></span> par 
-<span><?= $post->edited_at; ?></span>
+<span>Créé le <?= $post->created_at->format('d/m/Y H:i:s'); ?></span> par
+<span><?= isset($post->edited_at) && $post->edited_at->format('Y-m-d H:i'); ?></span>
 <span><?= $author->firstname; ?></span>
 <?php $content = ob_get_clean(); ?>
 
