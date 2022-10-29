eaa67c91012d1ebedf5ab189fa6d108a699029fe
dgueudre <dgueudre@gmail.com>
2022-10-29 15:24:10 +0200

    begin style

--- src/App/View/common.navbar.php
+++ src/App/View/common.navbar.php
@@ -1,9 +1,12 @@
-<ul>
-	<li><a href="/">Acceuil</a></li>
-	<?php if($user): ?>
-	<li><a href="/logout">Se déconnecter</a></li>
-	<li><a href="/posts/create">Créer un post</a></li>
-	<?php else: ?>
-	<li><a href="/login">Se connecter</a></li>
-	<?php endif; ?>
-</ul>
+<nav>
+	<span>dav-dev</span>
+	<ul>
+		<li><a href="/">Acceuil</a></li>
+		<?php if($user): ?>
+		<li><a href="/logout">Se déconnecter</a></li>
+		<li><a href="/posts/create">Créer un post</a></li>
+		<?php else: ?>
+		<li><a href="/login">Se connecter</a></li>
+		<?php endif; ?>
+	</ul>
+</nav>
